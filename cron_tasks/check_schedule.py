f2a87bbe980ba1d09ed7d6661ebad125b0618712
ximhan <ximhan@redhat.com>
2024-03-27 20:11:22 +0800

    add new api

--- cron_tasks/check_schedule.py
+++ cron_tasks/check_schedule.py
@@ -1,18 +1,23 @@
 
 import os
 import requests
+from typing import Optional
 import time
 from slack_sdk import WebClient
 
 slack_token = os.environ.get('SLACK_TOKEN', None)
+def post_slack_message(message: str, thread_ts: Optional[str] = None,):
+    response = WebClient(token=slack_token).chat_postMessage(
+                channel="#art-bot-monitoring",
+                text=message,
+                thread_ts=thread_ts, username="art-release-bot", link_names=True, attachments=[], icon_emoji=":dancing_robot:", reply_broadcast=False)
+    return response
 release_status = requests.get("https://art-dash-server-hackspace-ximhan.apps.artc2023.pc3z.p1.openshiftapps.com/api/v1/release_status").json()
 if release_status['alert'] != []:
-    response = WebClient(token=slack_token).chat_postMessage(
-            channel="#art-bot-monitoring",
-            text=' \n'.join([msg['status'] for msg in release_status['alert']]),
-            thread_ts=None, username="art-release-bot", link_names=True, attachments=[], icon_emoji=":dancing_robot:", reply_broadcast=False)
+    response = post_slack_message(' \n'.join([msg['status'] for msg in release_status['alert']]))
     print(f"message posted in https://redhat-internal.slack.com/archives/{response['channel']}/p{response['ts'].replace('.', '')}")
     if release_status['unshipped'] != []:
+        post_slack_message("start monitoring advisory not in shipped live status, interval set to 1 hour ...", thread_ts=response['ts'])
         while release_status['unshipped'] != []:
             for ad, msg in release_status['unshipped'].items():
                 # check ad status
@@ -20,11 +25,9 @@ if release_status['alert'] != []:
                 advisory_status = advisory_status_response['data'][-1]['attributes']['added']
                 if advisory_status == "SHIPPED_LIVE" or advisory_status == "DROPPED_NO_SHIP":
                     release_status['unshipped'].pop(ad)
-                    response = WebClient(token=slack_token).chat_postMessage(
-                        channel="#art-bot-monitoring",
-                        text=f"{msg} status changed to {advisory_status}",
-                        thread_ts=response['ts'], username="art-release-bot", link_names=True, attachments=[], icon_emoji=":dancing_robot:", reply_broadcast=False)
-            # sleep 6 hours
-            time.sleep(21600)
+                    post_slack_message(f"{msg} status changed to {advisory_status}", thread_ts=response['ts'])
+            # sleep 1 hours
+            print(f"sleeping 1 hours due to {release_status['unshipped']}")
+            time.sleep(3600)
 else:
     print("No alert", [msg['status'] for msg in release_status['message']])
