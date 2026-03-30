83870d1b396ba8d707434cefe92e780040462ebc
ximhan <ximhan@redhat.com>
2026-03-30 15:18:48 +0800

    add ship date api
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- cron_tasks/check_schedule.py
+++ cron_tasks/check_schedule.py
@@ -1,5 +1,7 @@
 
 import os
+import re
+import date
 import requests
 import time
 from typing import Optional
@@ -16,6 +18,48 @@ if releases_needs_prepare['releases'] != []:
     release_msg = "\n".join(f"• {msg[0]} : {msg[1]}, latest <https://amd64.ocp.releases.ci.openshift.org/releasestream/{msg[2]}.0-0.nightly/release/{msg[3]}|{msg[3]}> is {msg[4]}" for msg in releases_needs_prepare['releases'])
     post_slack_message(f"We need to prepare the following releases today:\n{release_msg}", thread_ts=None, channel="#ocp-sustaining-art-collaboration")
 
+def get_ga_version():
+    """
+    Get the latest GA version from https://github.com/openshift/cincinnati-graph-data/tree/master/channels
+    The highest version in the fast channel is considered GA.
+    """
+    headers = {"Authorization": f"token {os.environ['GITHUB_PERSONAL_ACCESS_TOKEN']}"}
+    response = requests.get(
+        "https://api.github.com/repos/openshift/cincinnati-graph-data/git/trees/master?recursive=1",
+        headers=headers
+    )
+
+    versions = []
+
+    for data in response.json()['tree']:
+        path: str = data['path']
+
+        regex = r"channels/fast-(?P<version>\d+.\d+).yaml"
+
+        if path.startswith("channels/fast"):
+            m = re.match(regex, path)
+            if m:
+                version: str = m.groupdict()['version']
+                versions.append(tuple(map(int, version.split("."))))
+    ga_version = sorted(versions, key=lambda x: (-x[0], -x[1]))[0]
+
+    return f"{ga_version[0]}.{ga_version[1]}"
+
+ga_version = get_ga_version()
+major, minor = ga_version.split(".")
+releases_need_to_ship = []
+# loop from ga version to previous until eol release, there is a treak that we look for previous 5 releases, so no need to connect github
+start_minor = 12
+versions = [f"{major}.{i}" for i in range(start_minor, int(minor) + 1)]
+for version in versions:
+    release_ship_schedule = requests.get(f"https://art-dash-server-hackspace-ximhan.apps.artc2023.pc3z.p1.openshiftapps.com/api/v1/release_schedule?type=release&branch_version={version}").json()
+    print(version)
+    print(release_ship_schedule)
+    for release in release_ship_schedule["all_ga_tasks"]:
+        if date.fromisoformat(release['date_finish']) == (date.today()):
+            releases_need_to_ship.append([release['name'].split(' ')[0], release['date_finish']])
+            break
+print(releases_need_to_ship)
 
 # check and monitor release advisory status, moved to konflux now
 # release_status = requests.get("https://art-dash-server-hackspace-ximhan.apps.artc2023.pc3z.p1.openshiftapps.com/api/v1/release_status").json()
